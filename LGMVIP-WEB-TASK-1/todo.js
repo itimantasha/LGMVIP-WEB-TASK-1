f5b9f71416b9ad1bc13f0412639adff60869e9ee
itimantasha <136356068+itimantasha@users.noreply.github.com>
2023-08-17 23:09:43 +0530

    Add files via upload

---
+++ LGMVIP-WEB-TASK-1/todo.js
@@ -0,0 +1,10 @@
+
+function Add(){
+    
+        let newele = document.createElement("ul");
+        inputs.value="";
+        newElement.querySelector("i").addeventlistner("click" , remove);
+        function remove (){
+            newele.remove()
+        }
+    }
